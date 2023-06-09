083983e37a360ebab457301ee3b26eb86069029e
osuchaya <suchaya.osatis@gmail.com>
2023-06-09 14:26:42 -0700

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +23,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+})};
+
+startApolloServer();
